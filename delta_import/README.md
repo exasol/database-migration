e321e1cfff719ae606550098f3a47f0bcf2551c8
exaValerieWiedemann <35218717+exaValerieWiedemann@users.noreply.github.com>
2018-05-02 15:46:36 +0200

    Updated delta-import-script and readme
    Added delta-detection for timestamps
    Updated corresponding readme

--- delta_import/README.md
+++ delta_import/README.md
@@ -1,56 +1,93 @@
 ## Delta import
-This script can be used to perform a delta import to load updated or new rows. To merge updated/new rows into the destination primary keys are used. Therefore, primary keys must be set in Exasol. The script works also with primary_keys that are in a deactivated state in Exasol.
+The delta-import script syncs tables from a source system to an Exasol database. It's made for use on a regular basis as only new or updated rows will be loaded from source to target.
 
-An update column has to be defined for the script. This column is used to determine new columns and therefore has to contain a value that increases for new values, e.g. an increasing job number or a timestamp.
+
+This script loads only the new rows from the source system. To merge updated/new rows into the destination, primary keys are used. Therefore, primary keys must be set in Exasol. The script works also with primary keys that are in a deactivated state in Exasol.
+
+An update column has to be defined for the script. This column is used to determine new columns and therefore has to contain a value that increases for new/changed values, e.g. an increasing job number or a timestamp.
+
+#### Prerequesites
+To use this script you need:
+- A source system (The script was tested on 'Oracle' and 'MySQL')
+- A target table that:
+    - has the same structure as the source table ( see [the import scripts](https://github.com/EXASOL/database-migration) for further instructions)
+    - has a primary key
+    - has a column which contains a value that is incremented for new/changed entries (e.g. a 'last updated' timestamp or an increasing job number)
+
+#### Installation
+- Download/ copy the [Query Wrapper](https://github.com/EXASOL/etl-utils/blob/master/query_wrapper.sql) from the ETL-Utils repository into EXAplus. The Query Wrapper is a library used for logging. Execute the create script statement. This will generate the *query_wrapper* script inside the schema 'ETL'.
+
+- Download/ copy the script [delta_import_on_primary_keys.sql](delta_import_on_primary_keys.sql) from above into EXAplus. Change the following line inside the script to point towards the *query_wrapper*-script if necessary.
+```sql
+import('ETL.QUERY_WRAPPER','QW')
+```
+
+- Once you're done with your changes, execute the create script statement.
+
+
+#### Script input parameters
+- **conn_type:** type of connection used to connect to the source system. Can be one of the connection types the import statement supports -> e.g. JDBC (to use with a generic database), EXA, ORA
+- **conn_db:** Type of source system database. Currently supports 'MYSQL', 'ORACLE' or 'EXASOL'
+- **conn_name:** connection name -> that connection has to be created upfront and is used for the import statements
+- **source_schema_name:** Name of the schema that contains the *source* table(s)
+- **target_schema_name:** Name of the schema that contains the *target* table(s)
+- **table_name:** Name of the target table(s), can contain wildcards
+- **delta_detection_column:** Name of the column in the target table that should be used for delta loading. The maximum value of this column is searched in the target table and only values greater than this value will be loaded from the source system. This could e.g. be an increasing job number in the source system or a 'last modified' timestamp.
+- **staging_schema:**  Name of a temporary schema used for storing data from the source system before merging them into the target table
+
+#### Script output
+Besides updating the target table, the script generates additioinal information about the loading process in two logging tables. They can be found inside the staging_schema.
+- __JOB_LOG:__ Contains information about the executed jobs, like execution time and status.
+- __JOB_DETAILS:__ Contains details on the loading process and the executed SQL statements. Use the following statement to see only entries that contain a log message:
+```SQL
+SELECT * FROM <staging_schema>.JOB_DETAILS
+WHERE LOG_LEVEL = 'LOG';
+```
 
 ## Example
-### Table in source system:
 
-| Id | First Name | Last Name | Department | Start_Date |
-|----|------------|-----------|------------|------------|
-| 1  | John       | Green     | Marketing  | 2015-08-15 |
-| 2  | Lucy       | Smith     | Sales      | 2017-07-01 |
-| 3  | Paul       | Taylor    | Sales      | 2017-08-01 |
-| 4  | Emma       | Williams  | Marketing  | 2018-01-15 |
-| 5  | Jacob      | Brown     | Sales      | 2018-02-01 |
+#### Table in target schema
 
+Table `EMPLOYEES` has a primary key on `ID`
 
-
-### Tables in target schema
-
-Table __employees__: Has no primary key
-
-| First Name | Last Name | Department | Start_Date |
-|------------|-----------|------------|------------|
-| John       | Green     | Marketing  | 2015-08-15 |
-| Lucy       | Smith     | Sales      | 2017-07-01 |
-| Paul       | Taylor    | Sales      | 2017-08-01 |
+| ID | FIRST_NAME | LAST_NAME | TELEPHONE_NR | LAST_UPDATED |
+|----|------------|-----------|--------------|--------------|
+| 1  | John       | Green     | 0123-45      | 2015-08-15   |
+| 2  | Lucy       | Smith     | 0123-46      | 2017-07-01   |
+| 3  | Paul       | Taylor    | 0123-47      | 2017-08-01   |
 
 
-Table __employees_with_pk__: Has a primary key on *id*
 
-| Id | First Name | Last Name | Department | Start_Date |
-|----|------------|-----------|------------|------------|
-| 1  | John       | Green     | Marketing  | 2015-08-15 |
-| 2  | Lucy       | Smith     | Sales      | 2017-07-01 |
-| 3  | Paul       | Taylor    | Sales      | 2017-08-01 |
+#### Table in source system:
+Data has changed since the last update of the target tables.
+Row 2 has been modified, row 4 and 5 have been added.
+
+| ID | FIRST_NAME | LAST_NAME | TELEPHONE_NR | LAST_UPDATED |
+|----|------------|-----------|--------------|--------------|
+| 1  | John       | Green     | 0123-45      | 2015-08-15   |
+| 2  | Lucy       | Smith     | **0987-65**  | **2018-02-01**     |
+| 3  | Paul       | Taylor    | 0123-47      | 2017-08-01         |
+| 4  | **Emma**   | **Williams** | **0123-48**   | **2018-01-15** |
+| 5  | **Jacob**  | **Brown** | **0123-49**      | **2018-02-01** |
 
 
-Execute delta loading for both tables with column *Start_Date* as update column:
+
+#### Performing the delta load:
+- Use column `LAST_UPDATED` as delta detection column:
 
 ```sql
 execute script database_migration.delta_import(
-'JDBC',  		-- conn_type (JDBC / EXA/...)
-'MYSQL', 		-- conn_db (ORACLE, MYSQL)
-'JDBC_MYSQL', 	-- conn_name
-'MY_SCHEMA_%',  -- schema_name (can contain wildcards)
-'%',     		-- table_name (can contain wildcards)
-'Start_Date',    		-- update_column
+'JDBC',  		   -- conn_type (JDBC / EXA/...)
+'MYSQL', 		   -- conn_db (ORACLE, MYSQL)
+'JDBC_MYSQL',     -- conn_name
+'MY_SOURCE_SCHEMA',	-- source_schema_name
+'MY_TARGET_SCHEMA',  -- target_schema_name
+'EMPLOYEES',     		-- table_name (can contain wildcards)
+'LAST_UPDATED',    		-- delta_detection_column
 'DELTA_STAGING' -- staging_schema_name
 );
 ```
 
-Result:
-- Table __employees__ will be truncated, all 5 rows will be loaded once again.
-- For table __employees_with_pk__ only the rows with id 4 and 5 will be loaded
-
+- The highest value in column `LAST_UPDATED` is *2017-08-01* --> Therefore only rows with a more recent update date will be considered
+- Rows with `ID` 4 and 5 will be added to the existing table
+- Row with `ID` 2 will be updated
--- delta_import/delta_import_on_primary_keys.sql
+++ delta_import/delta_import_on_primary_keys.sql
@@ -2,31 +2,25 @@
 --to merge updated rows into the destination table primary keys are used and are expected for the destination table (primary keys can be deactivated / only metadata)
 --in case there is no update column or primary keys -> a full load of the table is done.
 
---conn_type: connection type -> one of the connection types the import statement support -> e.g. JDBC (to use with a generic database), EXA, ORA
---conn_db: 	which database is this script importing from --> 'MYSQL' or 'EXASOL'
---conn_name: name of the connection -> that connection has to be created upfront and is used for the import statements
---schema_name: name of the schema -> can also include wildcards for a like-clause
---table_name: name of the table -> can include wildcard -> e.g. % to load the whole schema
---update_column: name of the column used for delta loading -> if that column exists new or modified rows have to have an increased value compared to the last load (max is used) -> e.g. use an increasing job number in the source system or a modified timestamp.
---staging_schema_name: name of a temporary schema used for storing tables into before merging them into the original tables
-create or replace script database_migration.delta_import(conn_type, conn_db, conn_name, schema_name, table_name, update_column, staging_schema_name) returns table
+-- conn_type: connection type -> one of the connection types the import statement support -> e.g. JDBC (to use with a generic database), EXA, ORA
+-- conn_db: 	which database is this script importing from --> 'MYSQL' or 'EXASOL'
+-- conn_name: name of the connection -> that connection has to be created upfront and is used for the import statements
+-- source_schema_name: name of the schema in the source system
+-- target_schema_name: name of the target schema (contains the table you want to import into) -> can also include wildcards for a like-clause
+-- table_name: name of the table -> can include wildcard -> e.g. % to load the whole schema
+-- delta_detection_column: name of the column used for delta loading -> if that column exists new or modified rows have to have an increased value compared to the last load (max is used) -> e.g. use an increasing job number in the source system or a modified timestamp.
+-- staging_schema: name of a temporary schema used for storing tables into before merging them into the original tables
+create or replace script database_migration.delta_import(conn_type, conn_db, conn_name, source_schema_name, target_schema_name, table_name, delta_detection_column, staging_schema) returns table
  as
 -----------------------------------------------------------------------------------------
 function quoteForDb(db_name, string_to_quote)
+-- remove quotes if already set
+string_to_quote = string.gsub(string_to_quote, '"', '')
 escape_char = '"'
 	if db_name == 'MYSQL' then
 		escape_char = '`'
 	end
 	return escape_char .. string_to_quote .. escape_char
-end
------------------------------------------------------------------------------------------
-function replaceQuotesForDb(db_name, string_to_requote)
-escape_char = '"'
-	if db_name == 'MYSQL' then
-		escape_char = '`'
-	end
-	newString = string.gsub(string_to_requote, '"', escape_char)
-	return newString
 end
 -----------------------------------------------------------------------------------------
 function debug(table)
@@ -49,27 +43,23 @@ function debug(table)
 	end
 	print('--------------------------------------------')
 end
------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------
--- create schema and tables for log data
-import('ETL.QUERY_WRAPPER','QW')
-wrapper = QW.new( 'ETL.JOB_LOG', 'ETL.JOB_DETAILS', 'delta_import_on_primary_keys')
 
+-----------------------------------------------------------------------------------------
+function create_log_tables(log_schema, job_log_table, job_details_table)
+
+query([[create schema if not exists ::s ]], {s= log_schema})
 query([[
-create schema if not exists ETL
-]])
-query([[
-create table if not exists ::s.job_log_delta_import(
+create table if not exists ::s.::jl(
 			run_id int identity,
 			script_name varchar(100),
 			status varchar(100),
 			start_time timestamp default systimestamp,
 			end_time timestamp
 		)
-]], {s= staging_schema_name})
+]], {s= staging_schema, jl=job_log_table})
 
 query([[
-		create table if not exists ::s.job_details_delta_import (
+		create table if not exists ::s.::jd (
 			detail_id int identity,
 			run_id int,
 			log_time timestamp,
@@ -77,20 +67,38 @@ query([[
 			log_message varchar(2000),
 			rowcount int
 		)
-]], {s= staging_schema_name})
+]], {s= log_schema, jd=job_details_table})
+end
+-----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 
-query([[create schema if not exists ::s ]], {s= staging_schema_name})
+
+-- create schema and tables for log data
+job_log_table= 'JOB_LOG'
+job_details_table = 'JOB_DETAILS'
+create_log_tables(staging_schema,job_log_table,job_details_table)
+
+-- import query wrapper and tell it logging table names
+import('ETL.QUERY_WRAPPER','QW')
+wrapper = QW.new( staging_schema..'.'..job_log_table, staging_schema..'.'..job_details_table, 'delta_import_on_primary_keys')
 
 -- setup all parameters
-
-wrapper:set_param('schema_filter',schema_name)
+wrapper:set_param('conn_type', conn_type)
+wrapper:set_param('conn_name', conn_name)
+wrapper:set_param('staging_schema',staging_schema)
+wrapper:set_param('target_schema',target_schema_name)
 wrapper:set_param('table_filter',table_name)
 
---query all needed information for schema name and table name within one resultset -> go through that resultset (res) afterwards
-suc, res = wrapper:query([[with constr as (
+delta_detection_column = quoteForDb('Exasol', delta_detection_column)
+wrapper:set_param('delta_col', delta_detection_column)
+
+
+--query all needed information for schema name and table name within one resultset -> go through resultset afterwards
+-- outputs: table_schema (target) table_name all_columns key_columns no_key_columns
+res_key_col_infos = wrapper:query_values([[with constr as (
 		select constraint_schema, constraint_table,	COLUMN_NAME
 		from EXA_ALL_CONSTRAINT_COLUMNS
-		where constraint_schema LIKE :schema_filter and
+		where constraint_schema LIKE :target_schema and
 			CONSTRAINT_TABLE LIKE :table_filter and
 			CONSTRAINT_TYPE = 'PRIMARY KEY'
 		order by 1,	2),
@@ -105,7 +113,7 @@ suc, res = wrapper:query([[with constr as (
 	all_tables as (
 		select *
 		from EXA_ALL_TABLES
-		WHERE TABLE_SCHEMA LIKE :schema_filter and
+		WHERE TABLE_SCHEMA LIKE :target_schema and
 			  TABLE_NAME LIKE :table_filter	),
 	tables_with_collist as (
 		select cols.column_schema schema_name, cols.column_table table_name, 
@@ -121,10 +129,10 @@ suc, res = wrapper:query([[with constr as (
 				no_key_cols.column_table = cols.column_table
 		group by 1,2)
 select
-	'"' || all_tables.table_schema || '"',
-	'"' || all_tables.table_name || '"',
-	key_columns,
+	'"' || all_tables.table_schema || '"' table_schema,
+	'"' || all_tables.table_name || '"' table_name,
 	all_columns,
+	key_columns,
 	no_key_columns
 from tables_with_collist
 	right join	all_tables on
@@ -134,79 +142,95 @@ order by 1,2 asc
 ]])
 --debug(res)
 
-wrapper:set_param('conn_type', conn_type)
-wrapper:set_param('conn_name', conn_name)
-wrapper:set_param('staging_schema',staging_schema_name)
 
 --iterate through the resultset
-for i=1,#res do 
+for target_schema, target_table, all_columns, key_columns, no_key_columns in res_key_col_infos do 
 
-	wrapper:set_param('curr_schema', res[i][1])
-	wrapper:set_param('curr_table', res[i][2])
+	wrapper:set_param('curr_schema', target_schema)
+	wrapper:set_param('curr_table', target_table)
 
-	if res[i][3] == NULL then
+	selectStarStmt = [[select * from ]]..quoteForDb(conn_db,source_schema_name)..[[.]]..quoteForDb(conn_db,target_table)
+	wrapper:set_param('selectStarStmt', selectStarStmt)
+ 	importFromSrc = [[ from ::conn_type at ::conn_name statement :selectStarStmt]]
+
+	if key_columns == NULL then
 		--no key column -> do a full load, no need for using the staging table
 		wrapper:query([[truncate table ::curr_schema.::curr_table]]);
-		suc, currRowCount = wrapper:query([[import into ::curr_schema.::curr_table from ::conn_type at ::conn_name statement 'select * from ]]..replaceQuotesForDb(conn_db,res[i][1])..[[.]]..replaceQuotesForDb(conn_db,res[i][2])..[[']])
+		suc, currRowCount = wrapper:query([[import into ::curr_schema.::curr_table]].. importFromSrc)
 		local tmpRowCount = currRowCount.etl_rows_written
-		wrapper:log('LOG',res[i][1]..'.'..res[i][2]..' has no key column -> TRUNCATE and LOAD', tmpRowCount)
+		wrapper:log('LOG',target_schema..'.'..target_table..' has no key column -> TRUNCATE and LOAD', tmpRowCount)
 
 	else
-		--there is a key column create a staging table and try to identify the maximum of the update column to load the delta
+		--there is a key column -> create a staging table and try to identify the maximum of the update column to load the delta
 		wrapper:query('create or replace table ::staging_schema.::curr_table like ::curr_schema.::curr_table');
-		-- TODO: Can we use the wrapper here? Don't want to throw an error if statement doesn't succeed
-		local success, maxRows = pquery('select max(::col_name) from ::curr_schema.::curr_table', {col_name=quote(update_column), curr_schema=res[i][1], curr_table=res[i][2]});
-		
-		selectStarStmt = [[select * from ]]..replaceQuotesForDb(conn_db,res[i][1])..[[.]]..replaceQuotesForDb(conn_db,res[i][2])
-		wrapper:set_param('selectStarStmt', selectStarStmt)
 
-		if not success then
-			output(maxRows.statement_text)
-			--error when selecting the max value -> there seems to be no update column -> do a truncate and then a load
-			wrapper:query([[truncate table ::curr_schema.::curr_table]]);
-			suc, currRowCount = wrapper:query([[import into ::curr_schema.::curr_table from ::conn_type at ::conn_name statement :selectStarStmt]])
-			local tmpRowCount = currRowCount.etl_rows_written
-			wrapper:log('LOG',res[i][1]..'.'..res[i][2]..' has key column, but no column '..update_column..' -> TRUNCATE and LOAD', tmpRowCount)
+		-- get type of delta_detection_column: if timestamp/date --> convert to predefined format
+		suc, delta_col_type = wrapper:query([[
+				select COLUMN_TYPE from EXA_ALL_COLUMNS 
+				where '"'|| column_schema || '"' = :curr_schema
+				and '"' || column_table || '"' = :curr_table
+				and '"' || column_name || '"' = :delta_col
+				]])
+		delta_col_type = delta_col_type[1][1]
+		
+		if (delta_col_type == 'TIMESTAMP' or delta_col_type == 'DATE') then
+			maxQuery = [[to_char(max(::col_name), 'YYYY-MM-DD HH24:MI:SS.FF6')]]
 		else
-			--querying maxcol was successful -> now do a load
-			if maxRows[1][1]==NULL then
-				--seems to be empty -> do a full load into the staging table
-				suc, currRowCount = wrapper:query([[import into ::staging_schema.::curr_table from ::conn_type at ::conn_name statement :selectStarStmt]])
+			maxQuery = 'max(::col_name)'
+		end
+		-- TODO: Can we use the wrapper here? Don't want to throw an error if statement doesn't succeed
+		local success, maxDeltaVal = pquery([[select ]].. maxQuery ..[[ from ::curr_schema.::curr_table]], {col_name=delta_detection_column, curr_schema=target_schema, curr_table=target_table});
+
+		if not success then	--delta-detection-column doesn't exist in the table-> do a truncate and then a load
+			wrapper:query([[truncate table ::curr_schema.::curr_table]]);
+			suc, currRowCount = wrapper:query([[import into ::curr_schema.::curr_table]]..importFromSrc)
+			local tmpRowCount = currRowCount.etl_rows_written
+			wrapper:log('LOG',target_schema..'.'..target_table..' has key column, but no column '..delta_detection_column..' -> TRUNCATE and LOAD', tmpRowCount)
+
+		else --querying max of delta detection column was successful -> load into staging table
+
+			if maxDeltaVal[1][1]== NULL then	-- no maximum in delta detection column found  -> do a full load into the staging table
+				suc, currRowCount = wrapper:query([[import into ::staging_schema.::curr_table]]..importFromSrc)
 				local tmpRowCount = currRowCount.etl_rows_written
-				wrapper:log('LOG',res[i][1]..'.'..res[i][2]..' is empty -> LOAD', tmpRowCount)
-			else
-				--only load the data into the staging table
-				selectDeltaStmt = [[select * from ]]..replaceQuotesForDb(conn_db, res[i][1])..[[.]]..replaceQuotesForDb(conn_db, res[i][2])..[[ where ]]..replaceQuotesForDb(conn_db, update_column)..[[ > ]]..maxRows[1][1]
+				wrapper:log('LOG',target_schema..'.'..target_table..' is empty -> LOAD', tmpRowCount)
+			
+			else -- load only new rows into the staging table
+				if (delta_col_type == 'TIMESTAMP' or delta_col_type == 'DATE') then
+					if(conn_db == 'MYSQL') then
+						maxDelta = [[STR_TO_DATE(']]..maxDeltaVal[1][1]..[[', '%Y-%m-%d %H:%i:%s.%f')]]
+					else
+						maxDelta = [[to_timestamp(']]..maxDeltaVal[1][1]..[[', 'YYYY-MM-DD HH24:MI:SS.FF6')]]
+					end
+				else
+					maxDelta = maxDeltaVal[1][1]
+				end
+
+				selectDeltaStmt = selectStarStmt ..[[ where ]]..quoteForDb(conn_db, delta_detection_column)..[[ > ]]..maxDelta
 				wrapper:set_param('selectDeltaStmt', selectDeltaStmt)
-				wrapper:set_param('max_cur_val', maxRows[1][1])
 
 				suc, currRowCount  = wrapper:query([[import into ::staging_schema.::curr_table from ::conn_type at ::conn_name STATEMENT :selectDeltaStmt]])
 				local tmpRowCount = currRowCount.etl_rows_written
-				wrapper:log('LOG',res[i][1]..'.'..res[i][2]..' has column '..update_column..' -> start loading at '..maxRows[1][1], tmpRowCount)
+				wrapper:log('LOG',target_schema..'.'..target_table..' -> start loading at ' ..delta_detection_column..':'..maxDeltaVal[1][1], tmpRowCount)
 			end
-
 			-- data is in staging table now -> merge the staging table into the normal table based on primary keys of the original table 
-			local key_columns    = res[i][3]
-			local all_columns 	 = res[i][4]
-			local no_key_columns = res[i][5]
 			wrapper:query([[merge into ::curr_schema.::curr_table a using ::staging_schema.::curr_table b on ]]..key_columns..
-					[[ when matched then update set ]]..no_key_columns..[[ when not matched then insert values (]]..all_columns..[[)]])
+					[[ when matched then update set ]]..no_key_columns..[[ when not matched then insert values (]]..all_columns..[[)  ]])
 
 			wrapper:query('drop table ::staging_schema.::curr_table');
 		end
 	end
-	--if there are huge load jobs which might take some time uncomment the following line to do a commit within this script after loading each table -> note that 
-	--wrapper:query([[commit]])
 end
 return wrapper:finish()
 /
 
 execute script database_migration.delta_import(
-'JDBC',  		-- conn_type (JDBC / EXA/...)
-'MYSQL', 		-- conn_db (ORACLE, MYSQL)
-'JDBC_MYSQL', 	-- conn_name
-'my_schema_%',  -- schema_name (can contain wildcards)
-'%',     		-- table_name (can contain wildcards)
-'id',    		-- update_column
-'DELTA_STAGING' -- staging_schema_name
-);
+'JDBC',  			-- conn_type (JDBC / ORA/ EXA/...)
+'MYSQL',	 		-- conn_db (ORACLE, MYSQL, EXASOL...)
+'MY_CONN', 			-- conn_name, case sensitive
+'SOURCE_SCHEMA',	-- source_schema_name (can NOT contain wildcards), case sensitive
+'TARGET_SCHEMA',  	-- target_schema_name (can contain wildcards), case sensitive
+'TABLE_%',     		-- table_name (can contain wildcards), case sensitive
+'LAST_UPDATED',		-- delta_detection_column case sensitive
+'DELTA_STAGING' 	-- staging_schema, used for log files and temporary data storage
+)
+;
