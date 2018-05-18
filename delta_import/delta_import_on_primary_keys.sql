58779ed438d8108165f68cb3c34d80d74b0b1d1e
exaValerieWiedemann <35218717+exaValerieWiedemann@users.noreply.github.com>
2018-05-18 18:07:53 +0200

    Restructured Delta-import script
    Put parts of script into methods to make it more readable
    Script can also be used for local delta loads now

--- delta_import/delta_import_on_primary_keys.sql
+++ delta_import/delta_import_on_primary_keys.sql
@@ -14,6 +14,7 @@ create or replace script database_migration.delta_import(conn_type, conn_db, con
  as
 -----------------------------------------------------------------------------------------
 function quoteForDb(db_name, string_to_quote)
+
 -- remove quotes if already set
 string_to_quote = string.gsub(string_to_quote, '"', '')
 escape_char = '"'
@@ -69,33 +70,63 @@ query([[
 		)
 ]], {s= log_schema, jd=job_details_table})
 end
+
 -----------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------
+-- helper function for script, not intended to be used as standalone
+-- following parameters must be set for wrapper:
+-- curr_schema	wrapper:set_param('curr_schema', 'MY_VALUE')
+-- curr_table	wrapper:set_param('curr_table', 'MY_VALUE')
+-- delta_col	wrapper:set_param('delta_col', 'MY_VALUE')
+-- following variables must be present:
+-- conn_db, delta_detection_column, target_schema, target_table
+function getMaxValue(conn_db, delta_detection_column, target_schema, target_table)
+
+	-- get type of delta_detection_column: if timestamp/date --> convert to predefined format
+	suc, delta_col_type = wrapper:query([[
+			select COLUMN_TYPE from EXA_ALL_COLUMNS 
+			where '"'|| column_schema || '"' = :curr_schema
+			and '"' || column_table || '"' = :curr_table
+			and '"' || column_name || '"' = :delta_col
+			]])
+
+	delta_col_type = delta_col_type[1][1]
+		
+	if (delta_col_type == 'TIMESTAMP' or delta_col_type == 'DATE') then
+		maxQuery = [[to_char(max(::col_name), 'YYYY-MM-DD HH24:MI:SS.FF6')]]
+	else
+		maxQuery = 'max(::col_name)'
+	end
+	-- TODO: Can we use the wrapper here? Don't want to throw an error if statement doesn't succeed
+	success, maxDeltaVal = pquery([[select ]].. maxQuery ..[[ from ::curr_schema.::curr_table]], {col_name=delta_detection_column, curr_schema=target_schema, curr_table=target_table});
+
+	if success and maxDeltaVal[1][1] ~= NULL then
+		if (delta_col_type == 'TIMESTAMP' or delta_col_type == 'DATE') then
+			if(conn_db == 'MYSQL') then
+				maxDelta = [[STR_TO_DATE(']]..maxDeltaVal[1][1]..[[', '%Y-%m-%d %H:%i:%s.%f')]]
+			else
+				maxDelta = [[to_timestamp(']]..maxDeltaVal[1][1]..[[', 'YYYY-MM-DD HH24:MI:SS.FF6')]]
+			end
+		else
+			maxDelta = maxDeltaVal[1][1]
+		end
+		return success, maxDelta
+	else
+		return success, NULL
+	end
+end
 
 
--- create schema and tables for log data
-job_log_table= 'JOB_LOG'
-job_details_table = 'JOB_DETAILS'
-create_log_tables(staging_schema,job_log_table,job_details_table)
+------------
+-- queries all needed information for tables inside target_schema matching table_filter within one resultset
+-- following parameters must be set for wrapper:
+-- target_schema 	wrapper:set_param('curr_schema', 'MY_VALUE')
+-- table_filter 	wrapper:set_param('curr_schema', 'MY_VALUE')
+--
+-- outputs a table consisting of:
+-- table_schema, table_name, all_columns, key_columns, no_key_columns
+function getTargetTableInformation()
 
--- import query wrapper and tell it logging table names
-import('ETL.QUERY_WRAPPER','QW')
-wrapper = QW.new( staging_schema..'.'..job_log_table, staging_schema..'.'..job_details_table, 'delta_import_on_primary_keys')
-
--- setup all parameters
-wrapper:set_param('conn_type', conn_type)
-wrapper:set_param('conn_name', conn_name)
-wrapper:set_param('staging_schema',staging_schema)
-wrapper:set_param('target_schema',target_schema_name)
-wrapper:set_param('table_filter',table_name)
-
-delta_detection_column = quoteForDb('Exasol', delta_detection_column)
-wrapper:set_param('delta_col', delta_detection_column)
-
-
---query all needed information for schema name and table name within one resultset -> go through resultset afterwards
--- outputs: table_schema (target) table_name all_columns key_columns no_key_columns
-res_key_col_infos = wrapper:query_values([[with constr as (
+	res_key_col_infos = wrapper:query_values([[with constr as (
 		select constraint_schema, constraint_table,	COLUMN_NAME
 		from EXA_ALL_CONSTRAINT_COLUMNS
 		where constraint_schema LIKE :target_schema and
@@ -140,23 +171,72 @@ from tables_with_collist
 		all_tables.table_name = tables_with_collist.table_name
 order by 1,2 asc
 ]])
---debug(res)
+	return res_key_col_infos
+end
+
+
+
+--------------------------- actual script -----------------------------------------------
+-----------------------------------------------------------------------------------------
+
+
+-- create schema and tables for log data
+job_log_table= 'JOB_LOG'
+job_details_table = 'JOB_DETAILS'
+create_log_tables(staging_schema,job_log_table,job_details_table)
+
+-- import query wrapper and tell it logging table names
+import('ETL.QUERY_WRAPPER','QW')
+wrapper = QW.new( staging_schema..'.'..job_log_table, staging_schema..'.'..job_details_table, 'delta_import_on_primary_keys')
+
+-- setup all parameters
+wrapper:set_param('conn_type', conn_type)
+wrapper:set_param('conn_name', conn_name)
+wrapper:set_param('staging_schema',staging_schema)
+wrapper:set_param('target_schema',target_schema_name)
+wrapper:set_param('table_filter',table_name)
+
+delta_detection_column = quoteForDb('EXASOL', delta_detection_column)
+wrapper:set_param('delta_col', delta_detection_column)
+
+-- check if script is used on local tables
+if string.upper(conn_type) == 'LOCAL' then
+	local_conn = true
+	conn_db = 'EXASOL'
+else
+	local_conn = false
+end
+
+
+
+
+res_key_col_infos = getTargetTableInformation()
+--debug(res_key_col_infos)
 
 
 --iterate through the resultset
 for target_schema, target_table, all_columns, key_columns, no_key_columns in res_key_col_infos do 
 
+	-- setup parameters
 	wrapper:set_param('curr_schema', target_schema)
 	wrapper:set_param('curr_table', target_table)
+	if local_conn then
+		insert_keyword = 'insert '
+		importFromSrc = [[ (select * from ]]..quoteForDb(conn_db,source_schema_name)..[[.]]..quoteForDb(conn_db,target_table).. [[ )]]
+	else
+		insert_keyword = 'import '
+		selectStarStmt = [[select * from ]]..quoteForDb(conn_db,source_schema_name)..[[.]]..quoteForDb(conn_db,target_table)
+		wrapper:set_param('selectStmt', selectStarStmt)
+		importFromSrc = [[ from ::conn_type at ::conn_name statement :selectStmt]]
+	end
+	wrapper:set_param('insert_keyword', insert_keyword)
 
-	selectStarStmt = [[select * from ]]..quoteForDb(conn_db,source_schema_name)..[[.]]..quoteForDb(conn_db,target_table)
-	wrapper:set_param('selectStarStmt', selectStarStmt)
- 	importFromSrc = [[ from ::conn_type at ::conn_name statement :selectStarStmt]]
+
 
 	if key_columns == NULL then
 		--no key column -> do a full load, no need for using the staging table
 		wrapper:query([[truncate table ::curr_schema.::curr_table]]);
-		suc, currRowCount = wrapper:query([[import into ::curr_schema.::curr_table]].. importFromSrc)
+		suc, currRowCount = wrapper:query(insert_keyword ..[[ into ::curr_schema.::curr_table]].. importFromSrc)
 		local tmpRowCount = currRowCount.etl_rows_written
 		wrapper:log('LOG',target_schema..'.'..target_table..' has no key column -> TRUNCATE and LOAD', tmpRowCount)
 
@@ -164,51 +244,38 @@ for target_schema, target_table, all_columns, key_columns, no_key_columns in res
 		--there is a key column -> create a staging table and try to identify the maximum of the update column to load the delta
 		wrapper:query('create or replace table ::staging_schema.::curr_table like ::curr_schema.::curr_table');
 
-		-- get type of delta_detection_column: if timestamp/date --> convert to predefined format
-		suc, delta_col_type = wrapper:query([[
-				select COLUMN_TYPE from EXA_ALL_COLUMNS 
-				where '"'|| column_schema || '"' = :curr_schema
-				and '"' || column_table || '"' = :curr_table
-				and '"' || column_name || '"' = :delta_col
-				]])
-		delta_col_type = delta_col_type[1][1]
-		
-		if (delta_col_type == 'TIMESTAMP' or delta_col_type == 'DATE') then
-			maxQuery = [[to_char(max(::col_name), 'YYYY-MM-DD HH24:MI:SS.FF6')]]
-		else
-			maxQuery = 'max(::col_name)'
-		end
-		-- TODO: Can we use the wrapper here? Don't want to throw an error if statement doesn't succeed
-		local success, maxDeltaVal = pquery([[select ]].. maxQuery ..[[ from ::curr_schema.::curr_table]], {col_name=delta_detection_column, curr_schema=target_schema, curr_table=target_table});
 
-		if not success then	--delta-detection-column doesn't exist in the table-> do a truncate and then a load
+		maxDeltaColExists, maxDelta = getMaxValue(conn_db, delta_detection_column, target_schema, target_table)
+
+		
+		if not maxDeltaColExists then	--delta-detection-column doesn't exist in target or target table is already empty -> do a truncate and then a load
 			wrapper:query([[truncate table ::curr_schema.::curr_table]]);
-			suc, currRowCount = wrapper:query([[import into ::curr_schema.::curr_table]]..importFromSrc)
+			suc, currRowCount = wrapper:query(insert_keyword ..[[ into ::curr_schema.::curr_table]]..importFromSrc)
 			local tmpRowCount = currRowCount.etl_rows_written
 			wrapper:log('LOG',target_schema..'.'..target_table..' has key column, but no column '..delta_detection_column..' -> TRUNCATE and LOAD', tmpRowCount)
 
 		else --querying max of delta detection column was successful -> load into staging table
 
-			if maxDeltaVal[1][1]== NULL then	-- no maximum in delta detection column found  -> do a full load into the staging table
-				suc, currRowCount = wrapper:query([[import into ::staging_schema.::curr_table]]..importFromSrc)
+			if maxDelta == NULL then	-- no maximum in delta detection column found  -> do a full load into the staging table
+				suc, currRowCount = wrapper:query(insert_keyword ..[[ into ::staging_schema.::curr_table]]..importFromSrc)
 				local tmpRowCount = currRowCount.etl_rows_written
 				wrapper:log('LOG',target_schema..'.'..target_table..' is empty -> LOAD', tmpRowCount)
 			
 			else -- load only new rows into the staging table
-				if (delta_col_type == 'TIMESTAMP' or delta_col_type == 'DATE') then
-					if(conn_db == 'MYSQL') then
-						maxDelta = [[STR_TO_DATE(']]..maxDeltaVal[1][1]..[[', '%Y-%m-%d %H:%i:%s.%f')]]
-					else
-						maxDelta = [[to_timestamp(']]..maxDeltaVal[1][1]..[[', 'YYYY-MM-DD HH24:MI:SS.FF6')]]
-					end
+
+				
+				-- setup parameters for (local) delta import
+				if local_conn then
+					importFromSrc = [[ (select * from ]]..quoteForDb(conn_db,source_schema_name)..[[.]]..quoteForDb(conn_db,target_table).. 
+					[[ where ]]..quoteForDb(conn_db, delta_detection_column)..[[ > ]]..maxDelta..
+					[[ )]]
 				else
-					maxDelta = maxDeltaVal[1][1]
+					-- no need to overwrite importFromSrc statement, only adapt the selectStmt
+					selectDeltaStmt = selectStarStmt ..[[ where ]]..quoteForDb(conn_db, delta_detection_column)..[[ > ]]..maxDelta
+					wrapper:set_param('selectStmt', selectDeltaStmt)
 				end
 
-				selectDeltaStmt = selectStarStmt ..[[ where ]]..quoteForDb(conn_db, delta_detection_column)..[[ > ]]..maxDelta
-				wrapper:set_param('selectDeltaStmt', selectDeltaStmt)
-
-				suc, currRowCount  = wrapper:query([[import into ::staging_schema.::curr_table from ::conn_type at ::conn_name STATEMENT :selectDeltaStmt]])
+				suc, currRowCount  = wrapper:query(insert_keyword ..[[ into ::staging_schema.::curr_table]] .. importFromSrc)
 				local tmpRowCount = currRowCount.etl_rows_written
 				wrapper:log('LOG',target_schema..'.'..target_table..' -> start loading at ' ..delta_detection_column..':'..maxDeltaVal[1][1], tmpRowCount)
 			end
@@ -224,7 +291,7 @@ return wrapper:finish()
 /
 
 execute script database_migration.delta_import(
-'JDBC',  			-- conn_type (JDBC / ORA/ EXA/...)
+'JDBC',  			-- conn_type (JDBC / ORA/ EXA/...) To load from another schema in the same DB: set conn_type to 'LOCAL', conn_db to '' and conn_name to ''
 'MYSQL',	 		-- conn_db (ORACLE, MYSQL, EXASOL...)
 'MY_CONN', 			-- conn_name, case sensitive
 'SOURCE_SCHEMA',	-- source_schema_name (can NOT contain wildcards), case sensitive
