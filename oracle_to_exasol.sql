d8e6d08616636e099d318c94c739918ae661e59a
exaValerieWiedemann <35218717+exaValerieWiedemann@users.noreply.github.com>
2019-12-19 10:58:01 +0100

    Modified Oracle-to-Exasol script to ignore views
    Scirpt migrated table and views, now only tables get migrated

--- oracle_to_exasol.sql
+++ oracle_to_exasol.sql
@@ -135,7 +135,8 @@ success, res = pquery([[with ora_cols as(
 
 	select * from(
 		import from ]]..CONNECTION_TYPE..[[ at ::c
-		statement 'select ]]..all_tab_cols..[[  from all_tab_columns where table_name in (select table_name from all_tables where owner ]]..SCHEMA_STR..[[ and table_name ]]..TABLE_STR..[[) and owner ]]..SCHEMA_STR..[[ and table_name ]]..TABLE_STR..[['
+		statement 'select ]]..all_tab_cols..[[  from all_tab_columns where table_name in (select table_name from all_tables where owner ]]..SCHEMA_STR..[[ and table_name ]]..TABLE_STR..[[) and owner ]]..SCHEMA_STR..[[ and table_name ]]..TABLE_STR..[[ AND owner || table_name NOT in
+                        (select owner || view_name from all_views)'
 				)
 			),
 	ora_base as ( --cast to correct types
